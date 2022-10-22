66d8695410ea55ccd6304d1266f6bf5c33aa54c8
Sudharsan-0-8 <dssudharsan07@gmail.com>
2022-10-22 17:54:07 +0530

    cors

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const http = require('http');
 const express = require('express');
 const { Server: ServerSocket } = require('socket.io');
 const { ApolloServer } = require('apollo-server-express');
+const cors = require('cors');
 
 const { typeDefs } = require('./schema/typedefs.js');
 const { resolvers } = require('./schema/resolvers.js');
 
 const app = express();
+app.use(cors());
 const httpServer = http.createServer(app);
 const io = new ServerSocket(httpServer , { 
     cors: {
-        origin: '*',
+        origin: ['http://localhost:3000'],
     }
  });
 const PORT = 3010;
