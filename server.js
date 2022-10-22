599bd1fcb70c52e990ab468df14441107975f828
Sudharsan-0-8 <dssudharsan07@gmail.com>
2022-10-22 12:10:21 +0530

    socket

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
+const http = require('http');
 const express = require('express');
+const { Server: ServerSocket } = require('socket.io');
 const { ApolloServer } = require('apollo-server-express');
 
 const { typeDefs } = require('./schema/typedefs.js');
 const { resolvers } = require('./schema/resolvers.js');
 
 const app = express();
+const httpServer = http.createServer(app);
+const io = new ServerSocket(httpServer , { 
+    cors: {
+        origin: '*',
+    }
+ });
 const PORT = 3010;
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
-apolloServer.start().then( ()=> apolloServer.applyMiddleware({ app }))
+apolloServer.start().then( () => apolloServer.applyMiddleware({ app }) );
 
 app.get('/test', (req , res) => {
     res.send('test');
