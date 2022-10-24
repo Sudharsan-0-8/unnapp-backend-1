018f4c7750004f83748e48ba2e060848033fbc43
Sudharsan-0-8 <dssudharsan07@gmail.com>
2022-10-24 22:03:52 +0530

    mock data

---
+++ data/data.js
@@ -0,0 +1,13 @@
+const contest = [
+    {
+        id: '',
+        name: 'Contest-1',
+        description: '',
+        type: 'MCQ',
+        // start_date: '2022-10-01',
+        // start_time: 
+        // duration: 
+        host: '',
+        no_of_participants: '',
+    }
+]
