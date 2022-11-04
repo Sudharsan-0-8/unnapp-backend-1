43b7a33223db75f482ac98c55cd07c112a1496c6
Sudharsan-0-8 <116365632+Sudharsan-0-8@users.noreply.github.com>
2022-11-04 20:26:40 +0530

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,13 +1,29 @@
-const contest = [
+const Contests = [
     {
-        id: '',
+        id: '1001',
         name: 'Contest-1',
         description: '',
         type: 'MCQ',
-        // start_date: '2022-10-01',
-        // start_time: 
-        // duration: 
-        host: '',
-        no_of_participants: '',
-    }
+        start_date: '2022-10-01',
+        start_time: '05:00 PM',
+        end_date: '2022-10-02',
+        end_time: '60:00 pm',
+        duration: 60,
+        host_id: '',
+        no_of_registrations: 0,
+        no_of_hosted: 0,
+    },
+]
+
+const Users = [
+    {
+        id: '',
+        username: '',
+        password: '',
+        no_of_participations: 0,
+        no_of_hosted: 0,
+        following_id: ['', ''],
+        hosted: ['', ''],
+        followers: ['', ''],
+    },
 ]
