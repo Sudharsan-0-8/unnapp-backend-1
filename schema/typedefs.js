9236fce5e885a39d66956c54fa57ba68f1876186
Sudharsan-0-8 <116365632+Sudharsan-0-8@users.noreply.github.com>
2022-11-04 20:26:05 +0530

    Update typedefs.js

--- schema/typedefs.js
+++ schema/typedefs.js
@@ -1,6 +1,74 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+
+    type option {
+        no: INT!
+        type: TYPEOFOPTION = NORMAL
+    }
+
+    type answer {
+        ques_id: ID
+        answer: String
+    }
+
+    type User {
+        id: ID!
+        username: String!
+        password: String!
+        no_of_participations: INT
+        no_of_hosted: INT
+        following_id: [ID]
+        hosted: [Contest]
+        followers: [User]
+        following: [User]
+    }
+
+    type Contest {
+        id: ID!
+        name: String!
+        description: String
+        type: TYPEOFCONTEST = MCQ
+        host_id: ID!
+        start_date: String!
+        start_time: String!
+        end_time: String!
+        end_date: String!
+        duration: INT
+        no_of_registrations: INT
+    }
+    
+    type Ques {
+        id: ID!
+        contes_id: ID!
+        question: String!
+        options: [option]
+    }
+
+    type Report {
+        user_id: ID!
+        contest_id: ID!
+        answers: [answer]
+        status: CONTESTSTATUS = TAKEN
+        marks: INT
+    }
+
+    enum TYPEOFCONTEST {
+        MCQ
+        CODE
+    }
+
+    enum TYPEOFOPTION {
+        NORMAL
+        STRINGINPUT
+    }
+
+    enum CONTESTSTATUS {
+        COMPLETED
+        TAKEN
+    }
+
+
     type Query {
         test: String!
     }
